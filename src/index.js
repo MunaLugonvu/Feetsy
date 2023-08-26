1332e2bcf917fd2d2508a3b6e2c15d1eb8e3915e
Lugonvu Mayimuna Kizza <munalugonvu@gmail.com>
2023-08-26 13:18:59 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import ErrorBoundary from "./errorBoundary"
+
+ReactDOM.render(<ErrorBoundary><App /></ErrorBoundary>, document.getElementById("root"));
