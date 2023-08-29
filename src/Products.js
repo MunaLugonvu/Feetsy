2c4c66662208351329977e21eb8dc9a87ceba7dd
Lugonvu Mayimuna Kizza <munalugonvu@gmail.com>
2023-08-29 13:39:37 +0300

    enable user pick shoe size

--- src/Products.js
+++ src/Products.js
@@ -36,16 +36,7 @@ export default function Products() {
      
           <section id="filters">
             <label htmlFor="size">Filter by Size:</label>{" "}
-            <select
-              id="size"
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
-            >
-              <option value="">All sizes</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
+           
             {size && <h2> Found {filterProd.length} items</h2>}
           </section>
           <section id="products"> {filterProd.map(renderProduct)}</section>
